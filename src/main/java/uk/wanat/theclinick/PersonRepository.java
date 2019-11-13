05636a3426986de02344ba548f34339d7c513488
Karolina Wanat <carolinawanat@gmail.com>
2019-11-13 19:54:30 +0000

    JPA dependency + test class

---
+++ src/main/java/uk/wanat/theclinick/PersonRepository.java
@@ -0,0 +1,6 @@
+package uk.wanat.theclinick;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
--- src/main/java/uk/wanat/theclinick/Start.java
+++ src/main/java/uk/wanat/theclinick/Start.java
@@ -1,13 +1,22 @@
 package uk.wanat.theclinick;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Start {
+    @Autowired
+    PersonRepository personRepository;
 
     @GetMapping("/hello")
     public String sayHello(){
         return "hello!!";
     }
+    @GetMapping("/show")
+    public Person addPerson(){
+        Person person = new Person("James");
+        return personRepository.save(person);
+    }
+
 }
