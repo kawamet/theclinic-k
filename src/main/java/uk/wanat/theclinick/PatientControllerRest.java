9978ac5bea49f2bab7b5917868efd0fe3b9dd65e
Karolina Wanat <carolinawanat@gmail.com>
2020-01-02 21:29:05 +0000

    appointment repo-impl + controller

--- src/main/java/uk/wanat/theclinick/PatientControllerRest.java
+++ src/main/java/uk/wanat/theclinick/PatientControllerRest.java
@@ -14,6 +14,8 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -30,8 +32,8 @@ public class PatientControllerRest {
 
 
     @GetMapping("/add")
-    public Patient addPatient() {
-        LocalDate birthDate = LocalDate.of(1968,9,25);
+    public Appointment addPatient() {
+      /*  LocalDate birthDate = LocalDate.of(1968,9,25);
         Date date = new Date();
         Patient patient = new Patient("Will", "Smith",Gender.MALE, "07776512547","SZ102030", birthDate );
         Address address = new Address("Westminster", "SW1A 0AA", "London");
@@ -42,7 +44,24 @@ public class PatientControllerRest {
         patient.setLogin(login);
         patient.setMedicalHistory(medicalHistory);
         patient.setAddress(address);
-        return patientRepository.save(patient);
+        return patientRepository.save(patient);*/
+
+      /*  LocalDate localDate = LocalDate.of(2010, 11, 19);
+        LocalTime localTime = LocalTime.of(10, 30);
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);*/
+        Appointment appointment = new Appointment();
+       /* Doctor doctor = new Doctor(Speciality.DENTISTRY, "Karol", "Brzuchomowca");
+        appointment.setDoctor(doctor);
+        Patient patient = new Patient("Karolina", "Mala", Gender.FEMALE, "07772516871", "sz123456", null);
+        appointment.setPatient(patient);
+        appointment.setTimeStart(localDateTime);
+        appointment.setTimeEnd(localDateTime.plusMinutes(30));
+        appointment.setExaminationRoom(ExaminationRoom.A2);
+        appointmentRepository.save(appointment);
+        doctor.add(appointment);
+        patient.add(appointment);
+*/
+        return appointment;
     }
 
     @RequestMapping(value = "/deletePatient", params = "id", method = GET)
@@ -57,11 +76,11 @@ public class PatientControllerRest {
     }
 
     @GetMapping("/addAppointment")
-    public void addAppointment(){
-        LocalDate localDate = LocalDate.of(2020,5,12);
+    public void addAppointment() {
+        LocalDate localDate = LocalDate.of(2020, 5, 12);
         LocalTime localTime = LocalTime.of(12, 30);
-        LocalDateTime appiotmentTime = LocalDateTime.of(localDate,localTime);
-        Appointment appointment = new Appointment(appiotmentTime,appiotmentTime.plusMinutes(30), ExaminationRoom.A2);
+        LocalDateTime appiotmentTime = LocalDateTime.of(localDate, localTime);
+        Appointment appointment = new Appointment(appiotmentTime, ExaminationRoom.A2);
         Doctor doctor = doctorRepository.findFirstById(1L);
         appointment.setDoctor(doctor);
         doctor.add(appointment);
